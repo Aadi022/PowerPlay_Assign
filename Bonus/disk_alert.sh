d662a5d90e23055a04b041f647be98fc38e81613
Aaditya Mehta <92634361+Aadi022@users.noreply.github.com>
2025-11-21 13:44:44 +0530

    Update disk_alert.sh

--- Bonus/disk_alert.sh
+++ Bonus/disk_alert.sh
@@ -2,9 +2,10 @@ THRESHOLD=80
 DISK_USE=$(df -h / | awk 'NR==2 {gsub("%",""); print $5}')
 
 if [ "$DISK_USE" -gt "$THRESHOLD" ]; then
-  MESSAGE="⚠️ ALERT: Disk usage is ${DISK_USE}% on $(hostname)"
+  MESSAGE=" ALERT: Disk usage is ${DISK_USE}% on $(hostname)"
   aws sns publish \
     --topic-arn arn:aws:sns:us-east-1:396913706461:disk-alert-topic \
     --message "$MESSAGE" \
     --region us-east-1
 fi
+
